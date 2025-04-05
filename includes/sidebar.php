bf26f9593efbe0065de51b0ab9653b62d0710113
Murat Büyükyılmaz <murat-halo@hotmail.com>
2025-04-05 19:55:33 +0300

    deneme

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -22,19 +22,18 @@
 
     <?php elseif ($role === 'club_president'): ?>
         <a href="index.php"><i class="bi bi-house-fill"></i> Ana Sayfa</a>
-        <a href="kulup.php"><i class="bi bi-people-fill"></i> Kulüpler</a>
-        <a href="message_room.php"><i class="bi bi-chat-left-text-fill"></i> İletişim</a>
         <a href="bildirim.php" class="position-relative">
             <i class="bi bi-bell-fill"></i> Bildirimler
             <span class="badge badge-notify">3</span>
         </a>
         <a href="etkinlik.php"><i class="bi bi-calendar-event-fill"></i> Etkinlikler</a>
+        <a href="Etkinlik_yaratma.php"><i class="bi bi-plus-circle-fill"></i> Etkinlik Yaratma</a>
+        <a href="duyuru_yaratma.php"><i class="bi bi-megaphone-fill"></i> Duyuru Yaratma</a>
         <a href="kulup_bilgileri(baskan).php"><i class="bi bi-info-circle-fill"></i> Kulüp Bilgileri</a>
         <a href="basvuru.php"><i class="bi bi-file-earmark-person-fill"></i> Başvurular</a>
 
     <?php elseif ($role === 'manager'): ?>
         <a href="index.php"><i class="bi bi-house-fill"></i> Ana Sayfa</a>
-        <a href="kulup.php"><i class="bi bi-people-fill"></i> Kulüpler</a>
         <a href="message_room.php"><i class="bi bi-chat-left-text-fill"></i> İletişim</a>
         <a href="bildirim.php" class="position-relative">
             <i class="bi bi-bell-fill"></i> Bildirimler
